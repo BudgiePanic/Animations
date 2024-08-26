415019048354e5d53d2accfdda8719d8c5425d28
BudgiePanic <bsanson90@gmail.com>
2024-08-27 10:47:39 +1200

    fix: initialize empty quaternion as scalar
    quaternions when used for rotation in 3d need to have a magnitude
    of 1. an empty quaternion initializes the imaginary components of the
    quaternion to zero and the real component to 1

--- rotation/quaternion.h
+++ rotation/quaternion.h
@@ -20,7 +20,7 @@ namespace rotation {
 			};
 			float v[4];
 		};
-		inline quaternion() : x(0.0f), y(0.0f), z(0.0f), w(0.0f) {}
+		inline quaternion() : x(0.0f), y(0.0f), z(0.0f), w(1.0f) {}
 		inline quaternion(float _x, float _y, float _z, float _w) : x(_x), y(_y), z(_z), w(_w) {}
 	};
 
