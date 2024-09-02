eb244013183a5352e15df4968ad96014d9ef83e9
BudgiePanic <bsanson90@gmail.com>
2024-09-02 12:00:05 +1200

    docs: describe purpose of index buffer

--- render/IndexBuffer.h
+++ render/IndexBuffer.h
@@ -7,6 +7,11 @@
 
 namespace render {
 
+/// <summary>
+/// Stores indices in a buffer.
+/// The primary use of the indices is to index into vertex array to allow vertex sharing.
+/// Instead of all triangles needing their own unique copies of vertices, they can share vertices, reducing space required to store a model.
+/// </summary>
 class IndexBuffer {
 public:
 	/// <summary>
