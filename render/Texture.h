ef73c3ea1d610b5766847648ef3c27d3af5fe32e
BudgiePanic <bsanson90@gmail.com>
2024-09-02 12:28:18 +1200

    feat: define texture class
    the texture class abstracts the loading of texture data from file and
    sending texture data to the GPU via the OpenGL library

---
+++ render/Texture.h
@@ -0,0 +1,43 @@
+#pragma once
+
+namespace render{
+
+class Texture {
+protected:
+	unsigned int mWidth;
+	unsigned int mHeight;
+	unsigned int mChannels;
+	unsigned int mFileHandle;
+private:
+	// Prevent aliasing of texture resources by hiding copy constructor and assignment operator
+	Texture(const Texture& other);
+	Texture& operator=(const Texture& other);
+
+public:
+	Texture();
+	Texture(const char* filePath);
+	~Texture();
+	/// <summary>
+	/// TODO
+	/// </summary>
+	/// <param name="filePath"></param>
+	void Load(const char* filePath);
+	/// <summary>
+	/// TODO
+	/// </summary>
+	/// <param name="uniform"></param>
+	/// <param name="textureIndex"></param>
+	void Set(unsigned int uniform, unsigned int textureIndex);
+	/// <summary>
+	/// TODO
+	/// </summary>
+	/// <param name="textureIndex"></param>
+	void UnSet(unsigned int textureIndex);
+	/// <summary>
+	/// TODO
+	/// </summary>
+	/// <returns></returns>
+	unsigned int GetHandle();
+};
+
+}
