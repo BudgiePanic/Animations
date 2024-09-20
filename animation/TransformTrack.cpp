07c57de36fd22045a09d989992d3fc80a1e7a729
BudgiePanic <bsanson90@gmail.com>
2024-09-20 17:54:07 +1200

    feat: impl SRTtrack start and end time getters

--- animation/TransformTrack.cpp
+++ animation/TransformTrack.cpp
@@ -1,4 +1,5 @@
 #include "TransformTrack.h"
+#include <limits>
 
 namespace anim{
 
@@ -15,6 +16,42 @@ TrackScalar& SRTtrack::GetScaleTrack() { return this->scale; }
 TrackQuaternion& SRTtrack::GetQuaternionTrack() { return this->rotation; }
 
 TrackVector& SRTtrack::GetTranslationTrack() { return this->translation; }
+
+float SRTtrack::GetStartTime() {
+	static_assert(std::numeric_limits<float>::is_iec559, "No IEEE 754 :(");
+	float scaleStart = std::numeric_limits<float>::infinity();
+	float rotationStart = scaleStart;
+	float translationStart = scaleStart;
+	if (this->scale.Size() > 1) {
+		scaleStart = this->scale.GetStartTime();
+	}
+	if (this->rotation.Size() > 1) {
+		rotationStart = this->rotation.GetStartTime();
+	}
+	if (this->translation.Size() > 1) {
+		translationStart = this->translation.GetStartTime();
+	}
+	float scaleRotationSmallest = (scaleStart < rotationStart ? scaleStart : rotationStart);
+	return translationStart < scaleRotationSmallest ? translationStart : scaleRotationSmallest;
+}
+
+float SRTtrack::GetEndTime() {
+	static_assert(std::numeric_limits<float>::is_iec559, "No IEEE 754 :(");
+	float scaleEnd = -1.0f * std::numeric_limits<float>::infinity();
+	float rotationEnd = scaleEnd;
+	float translationEnd = scaleEnd;
+	if (this->scale.Size() > 1) {
+		scaleEnd = this->scale.GetEndTime();
+	}
+	if (this->rotation.Size() > 1) {
+		rotationEnd = this->rotation.GetEndTime();
+	}
+	if (this->translation.Size() > 1) {
+		translationEnd = this->translation.GetEndTime();
+	}
+	float scaleRotationLargest = (scaleEnd > rotationEnd ? scaleEnd : rotationEnd);
+	return translationEnd > scaleRotationLargest ? translationEnd : scaleRotationLargest;
+}
 
 bool SRTtrack::hasValidTrack() {
 	bool result = false; 
