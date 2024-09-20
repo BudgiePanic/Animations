d6356f3308a75d135c5b605233c256c18aee7cad
BudgiePanic <bsanson90@gmail.com>
2024-09-20 17:23:10 +1200

    feat: create SRTtrack header

--- animation/TransformTrack.h
+++ animation/TransformTrack.h
@@ -1,5 +1,37 @@
 #pragma once
+#include "Track.h"
+#include "../transforms/srt.h"
 
 namespace anim {
-	
+
+	class SRTtrack {
+	protected:
+		/// <summary>
+		/// Can be either a bone ID or an object ID if the SRT track is being used to animate a non-skeletal entity
+		/// </summary>
+		unsigned int id;
+		TrackScalar scale;
+		TrackQuaternion rotation;
+		TrackVector translation;
+	public:
+		SRTtrack();
+		unsigned int GetID();
+		void SetID(unsigned int id);
+		TrackScalar& GetScaleTrack();
+		TrackQuaternion& GetQuaternionTrack();
+		TrackVector& GetTranslationTrack();
+		float GetStartTime();
+		float GetEndTime();
+		bool hasValidTrack();
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="referencePose">The reference pose is used if a sub-track cannot be sampled at the given sample time</param>
+		/// <param name="time">The time the track is sampled</param>
+		/// <param name="isTrackLooping"></param>
+		/// <returns></returns>
+		transforms::srt Sample(const transforms::srt& referencePose, float time, bool isTrackLooping);
+
+	};
+
 }
