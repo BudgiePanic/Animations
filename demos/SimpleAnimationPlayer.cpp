a9864b716e9ab902a11855b5f73525e3c65e1590
BudgiePanic <bsanson90@gmail.com>
2024-10-03 13:39:09 +1300

    feat: setup simple animation player implementation

---
+++ demos/SimpleAnimationPlayer.cpp
@@ -0,0 +1,51 @@
+#include "SimpleAnimationPlayer.h"
+
+namespace demos {
+
+	void SimpleAnimationPlayer::Initialize() {
+	}
+
+	void SimpleAnimationPlayer::ShutDown() {
+	}
+
+	void SimpleAnimationPlayer::Update(float deltaTime) {
+	}
+
+	void SimpleAnimationPlayer::Render(float aspectRatio) {
+	}
+
+	SimpleAnimationPlayer::SkeletonDrawer::SkeletonDrawer() {
+	}
+
+	SimpleAnimationPlayer::SkeletonDrawer::SkeletonDrawer(unsigned int size) {
+	}
+
+	SimpleAnimationPlayer::SkeletonDrawer::~SkeletonDrawer() {
+	}
+
+	unsigned int SimpleAnimationPlayer::SkeletonDrawer::Size() {
+		return 0;
+	}
+
+	void SimpleAnimationPlayer::SkeletonDrawer::Resize(unsigned int size) {
+	}
+
+	f3& SimpleAnimationPlayer::SkeletonDrawer::operator[](unsigned int index) {
+		return ;
+	}
+
+	void SimpleAnimationPlayer::SkeletonDrawer::Add(const f3& vector) {
+	}
+
+	void SimpleAnimationPlayer::SkeletonDrawer::PointsFromPose() {
+	}
+
+	void SimpleAnimationPlayer::SkeletonDrawer::UpdateBuffers() {
+	}
+
+	void SimpleAnimationPlayer::SkeletonDrawer::Draw(LineDrawMode mode, const f3& lineColor, const mat4f worldCameraProjection) 	{
+
+	}
+
+}
+
