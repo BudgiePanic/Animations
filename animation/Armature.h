138d00a361fb63c80345aa229f72aab2483f2352
BudgiePanic <bsanson90@gmail.com>
2024-10-10 15:31:28 +1300

    feat: declare armature container

---
+++ animation/Armature.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "Pose.h"
+#include "../Mat4f.h"
+
+namespace anim {
+
+	/// <summary>
+	/// An armature stores all the shared data between instances of animated meshes.
+	/// They store the rest pose, bind pose, inverse bind pose, and bone names.
+	/// The armature may also be known as a skeleton or a rig.
+	/// </summary>
+	class Armature {
+	protected:
+		Pose restPose;
+		/// <summary>
+		/// The bind pose matches to the model space vertices of the mesh when T-posed.
+		/// </summary>
+		Pose bindPose;
+		/// <summary>
+		/// Converts model space vertices to be in a space relative to a bone.
+		/// </summary>
+		std::vector<mat4f> inverseBindPose;
+		std::vector<std::string> boneNames;
+	protected:
+		/// <summary>
+		/// Recalculates the inverse bind pose. 
+		/// Call after updating the bindPose member variable.
+		/// </summary>
+		void RecalculateInverseBindPose();
+	public:
+		Armature();
+		Armature(const Pose& rest, const Pose& bind, const std::vector<std::string>& names);
+		void Set(const Pose& rest, const Pose& bind, const std::vector<std::string>& names);
+		Pose& GetBindPose();
+		Pose& GetRestPose();
+		std::vector<mat4f>& GetInverseBindPose();
+		std::vector<std::string>& GetBoneNames();
+		std::string& GetBoneName(unsigned int index);
+	};
+}
