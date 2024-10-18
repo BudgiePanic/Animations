fa34b7596d0d5e2654fefd2722603e64e30ca270
BudgiePanic <bsanson90@gmail.com>
2024-10-18 14:02:17 +1300

    feat: impl naive cpu mesh skinning

--- render/Mesh.cpp
+++ render/Mesh.cpp
@@ -55,6 +55,63 @@ std::vector<f4>& Mesh::GetBoneWeights() { return this->boneWeights; }
 
 std::vector<unsigned int>& Mesh::GetVertexIndices() { return this->vertexIndices; }
 
+void Mesh::Skin(anim::Armature& skeleton, anim::Pose& animatedPose) {
+	unsigned int vertCount = this->positions.size();
+	if (vertCount == 0) { return; } // no 'skin' to apply
+	this->skinnedPositions.resize(vertCount);
+	this->skinnedNormals.resize(vertCount);
+	anim::Pose& bindPose = skeleton.GetBindPose();
+	// skinning every vertex
+	for (int i = 0; i < vertCount; i++) {
+		// bones that influence this vertex
+		i4& bones = this->boneIndices[i]; 
+		// skin the vertex to the first bone
+		transforms::srt skinningTransform0 = transforms::combine(
+			animatedPose[bones.x], 
+			transforms::inverse(bindPose[bones.x])
+		);
+		f3 skinnedVert0 = transforms::applyPoint(skinningTransform0, this->positions[i]);
+		f3 skinnedNorm0 = transforms::applyVector(skinningTransform0, this->normals[i]);
+		// skin the vertex to the second bone
+		transforms::srt skinningTransform1 = transforms::combine(
+			animatedPose[bones.y],
+			transforms::inverse(bindPose[bones.y])
+		);
+		f3 skinnedVert1 = transforms::applyPoint(skinningTransform1, this->positions[i]);
+		f3 skinnedNorm1 = transforms::applyVector(skinningTransform1, this->normals[i]);
+		// skin the vertex to the third bone
+		transforms::srt skinningTransform2 = transforms::combine(
+			animatedPose[bones.z],
+			transforms::inverse(bindPose[bones.z])
+		);
+		f3 skinnedVert2 = transforms::applyPoint(skinningTransform2, this->positions[i]);
+		f3 skinnedNorm2 = transforms::applyVector(skinningTransform2, this->normals[i]);
+		// skin the vertex to the fourth bone
+		transforms::srt skinningTransform3 = transforms::combine(
+			animatedPose[bones.w],
+			transforms::inverse(bindPose[bones.w])
+		);
+		f3 skinnedVert3 = transforms::applyPoint(skinningTransform3, this->positions[i]);
+		f3 skinnedNorm3 = transforms::applyVector(skinningTransform3, this->normals[i]);
+		// combine skinned verts and normals into a final skinned vertex and normal
+		// how much each bone affects this vertex (if a bone has no influence, its weight will be zero)
+		f4& weights = this->boneWeights[i]; 
+		this->skinnedPositions[i] = 
+			skinnedVert0 * weights.x + 
+			skinnedVert1 * weights.y +
+			skinnedVert2 * weights.z +
+			skinnedVert3 * weights.w;
+		this->skinnedNormals[i] = 
+			skinnedNorm0 * weights.x +
+			skinnedNorm1 * weights.y +
+			skinnedNorm2 * weights.z +
+			skinnedNorm3 * weights.w;
+		// tell the GPU about the work we've done
+		this->positionAttribute->Set(this->skinnedPositions);
+		this->normalAttribute->Set(this->skinnedNormals);
+	}
+}
+
 void Mesh::SyncOpenGL() {
 	if (this->boneIndices.size() > 0) {
 		this->boneIndexAttribute->Set(this->boneIndices);
