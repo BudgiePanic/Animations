0060cc2b13757b5ddd6fae8a623bdc79dd27e85b
BudgiePanic <bsanson90@gmail.com>
2025-01-05 16:27:05 +1300

    feat: implement pose blending

--- animation/Pose.cpp
+++ animation/Pose.cpp
@@ -131,4 +131,16 @@ namespace anim {
 		}
 		return false;
 	}
+
+	void Blend(Pose& poseOut, Pose& a, Pose& b, float t, int rootBone) {
+		unsigned int numbBones = poseOut.Size();
+		for (unsigned int bone = 0; bone < numbBones; bone++) {
+			// negative root bone is assume to mean the caller wants blending applied to every bone
+			if (rootBone >= 0 && !IsBoneChildOf(poseOut, rootBone, bone)) { continue; }
+			// blend bones in local pose space
+			poseOut.SetLocalTransform(bone, 
+				transforms::mix(a.GetLocalTransform(bone), b.GetLocalTransform(bone), t)
+			);
+		}
+	}
 }
