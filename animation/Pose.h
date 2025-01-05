86e1f776d91d64907a1f907570d8a514c04c2668
BudgiePanic <bsanson90@gmail.com>
2025-01-05 16:11:53 +1300

    feat: declare new pose function headers
    these functions will be used to enable animation blending

--- animation/Pose.h
+++ animation/Pose.h
@@ -51,4 +51,23 @@ namespace anim {
 		bool operator==(const Pose& other);
 		bool operator!=(const Pose& other);
 	};
+
+	/// <summary>
+	/// Checks if a bone is a child of another bone in a pose bone hierarchy
+	/// </summary>
+	/// <param name="pose"></param>
+	/// <param name="parentBone"></param>
+	/// <param name="boneToCheck"></param>
+	/// <returns>true if boneToCheck is a child of parentBone in the pose bone hierarchy</returns>
+	bool IsBoneChildOf(Pose& pose, unsigned int parentBone, unsigned int boneToCheck);
+
+	/// <summary>
+	/// Combine two poses together. Assumes the bone hierarchy in each pose is the same.
+	/// </summary>
+	/// <param name="poseOut">Where the resulting blended pose should be written to</param>
+	/// <param name="a">first pose to combine</param>
+	/// <param name="b">second pose to combine</param>
+	/// <param name="t">percentage amount of pose 'b' to include. i.e. 0 implies all pose 'a', no pose 'b'</param>
+	/// <param name="rootBone">The starting bone of pose B which is blended into pose 'a'. i.e. left arm to only apply blend to an arm</param>
+	void Blend(Pose& poseOut, Pose& a, Pose& b, float t, int rootBone);
 }
