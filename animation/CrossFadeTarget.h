e0690817eaf36bf13972c16a200efc0381e16f6b
BudgiePanic <bsanson90@gmail.com>
2025-01-05 16:41:42 +1300

    feat: define cross fade target struct

---
+++ animation/CrossFadeTarget.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Pose.h"
+#include "Clip.h"
+
+namespace anim {
+
+	/// <summary>
+	/// Data courier containing information about the animation that is being faded out for another animation
+	/// Used in animation crossfading
+	/// </summary>
+	struct CrossFadeTarget {
+		Pose pose;
+		// The animation clip that is being faded out
+		Clip* clip;
+		float time;
+		float duration;
+		float elapsed;
+		CrossFadeTarget() : clip(NULL), time(0.0f), duration(0.0f), elapsed(0.0f) {}
+		CrossFadeTarget(Clip* target, Pose& pose, float dur) : clip(target), time(target->GetStartTime()), duration(dur), elapsed(0.0f) {}
+	};
+
+}
