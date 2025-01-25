3abf3489209c41b33f06a69b2335619a836a9456
BudgiePanic <bsanson90@gmail.com>
2025-01-25 13:08:52 +1300

    feat: impl dual quaternion skinning vertex shader

--- resource/shaders/skinned_dual_quat_vert.glsl
+++ resource/shaders/skinned_dual_quat_vert.glsl
@@ -1,6 +1,7 @@
 #version 330 core
 #define max_bones 120
 #define real_component 0 // the real component of a dual quaternion encodes rotation information
+#define dual_component 1 // the dual component encodes the position information
 // vertex shader for skinning using dual quaternions instead of 4x4 matrices
 // dual quaternions do not contain scale information, so an animation that uses scaling will render poorly with this vertex shader
 uniform mat4 model; // model space to world space transform
@@ -24,39 +25,68 @@ out vec3 fragment_world_position;
 out vec2 uv;
 
 /**
- * multiply two quaternions together
+ * multiply two quaternions together.
+ * Taken from quaternion.cpp
  */
 vec4 qMul(vec4 qLeft, vec4 qRight) {
-  // TODO
+  return vec4(
+	  (qRight.x * qLeft.w) + (qRight.y * qLeft.z) - (qRight.z * qLeft.y) + (qRight.w * qLeft.x),
+	  -(qRight.x * qLeft.z) + (qRight.y * qLeft.w) + (qRight.z * qLeft.x) + (qRight.w * qLeft.y),
+	  (qRight.x * qLeft.y) - (qRight.y * qLeft.x) + (qRight.z * qLeft.w) + (qRight.w * qLeft.z),
+	  -(qRight.x * qLeft.x) - (qRight.y * qLeft.y) - (qRight.z * qLeft.z) + (qRight.w * qLeft.w)
+	);
 }
 
 /**
  * normalize a dual quaternion
  */
 mat2x4 normalizeDualQ(mat2x4 dualQ) {
-  // TODO
+  float invMag = 1.0 / length(dualQ[real_component]);
+  dualQ[real_component] *= invMag;
+  dualQ[dual_component] *= invMag;
+  return dualQ;
 }
 
 /**
  * combine two dual quaternions together. 
- * used in the same way martix transforms are combined by multiplying them together
+ * used in the same way martix transforms are combined by multiplying them together.
+ * taken from DualQUaternion.cpp *operator
  */
 mat2x4 combineDualQ(mat2x4 left, mat2x4 right) {
-  // TODO
+  left = normalizeDualQ(left);
+  right = normalizeDualQ(right);
+  vec4 real = qMul(left[real_component], right[real_component]);
+  vec4 dual = qMul(left[real_component], right[dual_component]) + qMul(left[dual_component], right[real_component]);
+  return mat2x4(real, dual);
 }
 
 /**
- * Apply a dual quaternion transform to a vector
+ * Apply a dual quaternion transform to a vector.
+ * Taken from quaternion.cpp *operator
  */
 vec4 applyVector(mat2x4 dualQ, vec3 vector) {
-  // TODO
+  vec4 real = qualQ[real_component]; // rotation data
+  vec3 realVector = real.xyz;
+  float realScalar = real.w;
+
+  // dot product of a vector with itself is just the magnitude squared
+  vec3 v = realVector * 2.0f * dot(realVector, vector) + 
+  vector * (realScalar * realScalar - dot(realVector, realVector)) +
+  cross(realVector, vector) * 2.0f * realScalar;
+  return vec4(v, 0.0);
 }
 
 /**
- * Apply a dual quaternion transform to a point
+ * Apply a dual quaternion transform to a point.
+ * Taken from DualQuaternion.cpp applyPoint
  */
 vec4 applyPoint(mat2x4 dualQ, vec3 point) {
-  // TODO
+  vec4 real = dualQ[real_component];
+  vec4 dual = dualQ[dual_component];
+  vec4 conjugate = vec4(-real.xyz, real.w);
+  vec4 translation = qMul(conjugate, dual * 2.0).xyz;
+  vec3 v = applyVector(dualQ, point).xyz;
+  return vec4(v + translation, 1);
 }
 
 void main() {
